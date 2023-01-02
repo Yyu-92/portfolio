c8c26259c80de09f04ddd29d23e316ea91477109
Yyu-92 <110922447+Yyu-92@users.noreply.github.com>
2023-01-02 23:56:32 +0900

    Add files via upload

--- js/thema/portfolio.js
+++ js/thema/portfolio.js
@@ -1,30 +1,13 @@
-$(function(){
-	
-	//scroll시 class 추가
-	
- 	 $(window).scroll(function(){
-     	 if ($(this).scrollTop() > 90) {
-         $('.logo').addClass('act');
-         $('nav').addClass('act');
-      } else {
-         $('.logo').removeClass('act');
-         $('nav').removeClass('act');
-      }
-     
+
+// 더보기 창 끄기
+let closeList = [].slice.call(document.getElementsByClassName('close-btn'));
+let urlList = [].slice.call(document.getElementsByClassName('fixed-url'));
+function clickClose(){
+	closeList.forEach(function(c){
+		c.addEventListener('click', function(){
+			this.parentElement.parentElement.style.display = 'none';
+		});
 	});
-	
-});
-
-$(function(){
-	// click 시 가장 상단으로 이동
-    
-	$('.act').click(function(){
-	    $('html, body').animate({scrollTop : 0},600);
-	    return false;
-	}); 
-});
-
-
-function showFunction(){
-	document.getElementsByClassName('skill_list')[0].style.display = "block";
 }
+
+clickClose();
